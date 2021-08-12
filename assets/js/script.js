b5a6b69c29d238b722076f99eb90a23a1544c5ce
Damerian Dark <newbmailz@gmail.com>
2021-08-12 14:48:07 -0500

    Code works all done need to update  readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,77 @@
 let now = moment();
 let currentDay = $('#currentDay');
-let checkIt;
 let container = document.querySelector('.container');
-let saveIt = document.querySelectorAll('.saveBtn');
 let el;
 let presentHour;
 let currentHour;
-let storeTime;
-let storeThingy;
+let storeTimes;
+let storeNotes;
 let timeDay;
 let toDo;
-let datData;
-let textData;
+let storageArray = [{
+    hour: "9AM",
+    message: "" ,
+ },
+ {
+     hour: "10AM",
+     message: "" ,
+  },
+  {
+     hour: "11AM",
+     message: "" ,
+  },
+  {
+     hour: "12PM",
+     message: "" ,
+  },
+  {
+     hour: "1PM",
+     message: "" ,
+  },
+  {
+     hour: "2Pm",
+     message: "" ,
+  },
+  {
+     hour: "3PM",
+     message: "" ,
+  },
+  {
+     hour: "4PM",
+     message: "" ,
+  },
+  {
+     hour: "5PM",
+     message: "",
+  },];
+let tempVarArray;
 setInterval(getHour, 1000);
 setInterval(checkHour, 1000);
 
 init();
+                        
+                        function init(){
+                            if (
+                                localStorage === undefined ||
+                                localStorage.length === 0 ) {
+                                    storeNotes = JSON.parse(localStorage.getItem("toDo"));
+                                    storageArray = storeNotes;
+                                } else {
+                                    storeNotes = JSON.parse(localStorage.getItem("toDo"));
+                                    storageArray = storeNotes;
+                                }
+                                if (storageArray === undefined || storageArray === null){
+                            
+                                } else{
+                                for (i = 0; i < 9; i++) {
+                                    let newstorageArray = JSON.parse(localStorage.getItem("storageArray"));    
+                                    let theMessage = storageArray[i].message;
+                                    container.children[i].children[1].value = theMessage;
+                                }
+                                  }    
 
-function init(){
-if (
-    localStorage === undefined ||
-    localStorage.length === 0 ) {
-    storeTime = localStorage.setItem("timeDay", JSON.stringify([]));
-    storeThingy = localStorage.setItem("toDo", JSON.stringify([]));
-    storeTime = JSON.parse(localStorage.getItem("timeDay"));
-    storeThingy = JSON.parse(localStorage.getItem("toDo"));
-  } else {
-    storeTime = JSON.parse(localStorage.getItem("timeDay"));
-    storeThingy = JSON.parse(localStorage.getItem("toDo"));
-  }
 }
+
 
 function getHour(){
     let rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -40,11 +82,6 @@ function checkHour(){
    
     currentHour = moment().hour();
    
-    /*This function will need to pull the current hour
-    then it will need to evaluate which element with class hour matches
-    that same number. If it matches then it will get present class added
-    if it is below then it will get a past class added, and if it's above then
-    it will get future class added need to also make sure the class isn't already there before adding*/ 
  for (let i=0; i < 9; i++ ){
  el = container.children[i].children[1];
  presentHour = el.getAttribute('data-hour');
@@ -66,6 +103,7 @@ function checkHour(){
     if (presentHour.includes('past')){
 
     }else{
+        el.classList.remove('present');
         el.classList.add('past');  
     }
  }
@@ -74,57 +112,30 @@ function checkHour(){
     
 
 }
+
 document.querySelectorAll('.saveBtn').forEach(item => {
     item.addEventListener('click', event => { 
-        console.log("you saved me");
-         let textAr = event.target.previousElementSibling;
-         textData = textAr.value;
+        let textAr = event.target.previousElementSibling;
+         toDo = textAr.value;
          let datAr = textAr.previousElementSibling;
-         datData = datAr.innerText;
-         console.log(textAr);
-         console.log(textData);
-         console.log(datAr);
-         console.log(datData);
+         timeDay = datAr.innerText;
+         if (storageArray != null){
 
-         storeTime.push(textData);
-         storeThingy.push(datData);
-         localStorage.setItem(datAr, JSON.stringify(storeTime));
-         localStorage.setItem("message", JSON.stringify(storeThingy));
-
-
-      })
-})
-//      event.preventDefault(); 
+         
+for (let i = 0; i < storageArray.length; i++) {
+    let tempVar = container.children[i].children[0]
+    if (timeDay === tempVar.innerText){
+        let eventMessage = storageArray[i];
+        eventMessage.message = toDo;
+        let newEventMessage = eventMessage.message;
+    }
     
-//      console.log(event);
-//     let textAr = event.target.sibling[1];
-//      let datAr = event.target.sibling[0];
-//      console.log(textAr);
-//      console.log(datAr);
+}
+        localStorage.setItem("toDo", JSON.stringify(storageArray));
 
-// });
+         }else{
 
-
-// saveIt.forEach(el => el.addEventListener("click", event => {
-//   let textAr = event.target.sibling[1];
-//   let datAr = event.target.sibling[0];
-//   console.log(textAr);
-//   console.log(datAr);
-// //     timeDay = el.sibling[0].innerText;
-// //     console.log(timeDay);
-// //     toDo = el.sibling[1].value;
-// //   console.log(toDo);
-
-//     // if (userName === "") {
-//     //     displayMessage("error", "userName cannot be blank");
-//     // } else  {
-//     //     displayMessage("success", "Registered successfully");
-//     //     storeUser.push(userName);
-//     //     storeScore.push(daScore);
-//     //     //local storage is storing it to index 1 and 2 and not moving from there.
-//     //     localStorage.setItem("userName", JSON.stringify(storeUser));
-//     //     localStorage.setItem("score", JSON.stringify(storeScore));
-//     //     showWinners();
-
-//     // }
-// }));
+         }
+      })
+    
+})
---
+++ readme.md
@@ -0,0 +1,108 @@
+
+
+<!--
+*** Thanks for checking out the Best-README-Template. If you have a suggestion
+*** that would make this better, please fork the repo and create a pull request
+*** or simply open an issue with the tag "enhancement".
+*** Thanks again! Now go create something AMAZING! :D
+***
+***
+***
+*** To avoid retyping too much info. Do a search and replace for the following:
+*** github_username, repo_name, twitter_handle, email, project_title, project_description
+-->
+
+
+
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+
+
+
+
+<!-- PROJECT LOGO -->
+<br />
+
+
+  <h3 align="center">Trivia Game</h3>
+
+  <p align="center">
+     The game is simple answer as many questions as you can before the time is up. Wrong answers cost you to lose 5 seconds and correct answers scores you a point. At the end you can save your score to try and beat it the next time or play against family and friends.
+    <br />
+    <a href="https://github.com/Damerian00/QuizBowl"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://damerian00.github.io/QuizBowl/">View Demo</a>
+    
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+  This project was to create a trivia game that utilizes flexbox and local storage. The game is simple answer as many questions as you can before the time is up. Wrong answers cost you to lose 5 seconds and correct answers scores you a point. At the end you can save your score in local storage to try and beat at a later date. Currently it displays the first 6 scores stored in local storage. There is also a button to clear out your local storage so you can make new saves.
+
+
+
+![Project Preview](./assets/img/site_ss.png)
+
+
+
+
+
+### Built With
+
+* Visio Studio Code
+
+
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Patrick - (https://github.com/Damerian00) - GitHub Name
+
+Project Link: [https://github.com/Damerian00/QuizBowl]
+
+
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/github_username/repo.svg?style=for-the-badge
+[contributors-url]: https://github.com/github_username/repo/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/github_username/repo.svg?style=for-the-badge
+[forks-url]: https://github.com/github_username/repo/network/members
+[stars-shield]: https://img.shields.io/github/stars/github_username/repo.svg?style=for-the-badge
+[stars-url]: https://github.com/github_username/repo/stargazers
+[issues-shield]: https://img.shields.io/github/issues/github_username/repo.svg?style=for-the-badge
+[issues-url]: https://github.com/github_username/repo/issues
+[license-shield]: https://img.shields.io/github/license/github_username/repo.svg?style=for-the-badge
+[license-url]: https://github.com/github_username/repo/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/github_username
